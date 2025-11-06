4da02ff16343acdccd56a8506e745ab2b1651456
Héctor Ríos <JHector317031@gmail.com>
2025-11-06 14:09:37 -0500

    feat: adding infrastructure of reminders

---
+++ Contexts/Reminders/Infraestructure/ReminderRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using learning_center_webapi.Contexts.Reminders.Domain.Infraestructure;
+using learning_center_webapi.Contexts.Reminders.Domain.Model.Aggregates;
+using learning_center_webapi.Contexts.Shared.Infraestructure.Persistence.Configuration;
+using learning_center_webapi.Contexts.Shared.Infraestructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace learning_center_webapi.Contexts.Reminders.Infraestructure;
+
+public class ReminderRepository : BaseRepository<Reminder>, IReminderRepository
+{
+    private readonly LearningCenterContext _context;
+
+    public ReminderRepository(LearningCenterContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Reminder>> GetAllAsync()
+    {
+        return await _context.Set<Reminder>().ToListAsync();
+    }
+
+    public async Task<IEnumerable<Reminder>> GetByUserIdAsync(int userId)
+    {
+        var userReminders = await _context.Set<Reminder>()
+            .Where(r => r.UserId == userId)
+            .ToListAsync();
+        
+        return userReminders
+            .OrderByDescending(r => r.Date.Value)
+            .ThenByDescending(r => r.Time.Value)
+            .ToList();
+    }
+}
